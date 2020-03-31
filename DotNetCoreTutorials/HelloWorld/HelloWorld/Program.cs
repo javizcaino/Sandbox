58c105227bb44015290dd423aefb52f8f2a37d7d
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-03-31 18:07:45 +0200

    Creación de su primera aplicación de consola con .NET Core en Visual Studio 2019

---
+++ DotNetCoreTutorials/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    using System;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            string name = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
