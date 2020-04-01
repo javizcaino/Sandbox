0742cab9036f982dbee049b318c965746242afbc
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-04-01 17:45:17 +0200

    Segunda tanda de tutoriales
    Añadido tutorial de librería de clases con VS2019, proyecto de pruebas unitarias con VS2019, aplicación de consola con VSCode y libreria de clases con VSCode

---
+++ DotNetCoreTutorials/ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ShowCase
+{
+    using System;
+
+    using UtilityLibraries;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int row = 0;
+
+            while (true)
+            {
+                if (row == 0 || row >= 25)
+                {
+                    ResetConsole();
+                }
+
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Input: {input} {"Begins with uppercase? ",30}: " +
+                                  $"{(input.StartsWithUpper() ? "Yes" : "No")}\n");
+                row += 3;
+            }
+
+            return;
+
+            // Declare a ResetConsole local method
+            void ResetConsole()
+            {
+                if (row > 0)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                Console.WriteLine("\nPress <Enter> only to exit; otherwise, enter a string and press <Enter>:\n");
+                row = 3;
+            }
+        }
+    }
+}
---
+++ DotNetCoreTutorials_VSCode/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+        }
+    }
+}
