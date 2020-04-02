4efc48ac3f430bb80c19fd90c138333dabb28c96
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-04-02 16:00:48 +0200

    Added data annotations and updated database model

--- DotNetCoreTutorials/RazorPagesMovie/Models/Movie.cs
+++ DotNetCoreTutorials/RazorPagesMovie/Models/Movie.cs
@@ -10,19 +10,28 @@
 
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        public string Genre { get; set; }
-
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "money")]
         public decimal Price { get; set; }
 
-        public string Rating { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "This is not a valid rating")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
         #endregion Public Properties
     }
 }
