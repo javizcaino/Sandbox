d1d653f7a4fc76d9495c574ef285b9bfeb760dbc
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-04-02 13:08:10 +0200

    Added filtering

--- DotNetCoreTutorials/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ DotNetCoreTutorials/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿namespace RazorPagesMovie.Pages.Movies
 {
+    using System.Linq;
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.EntityFrameworkCore;
 
     using RazorPagesMovie.Data;
@@ -11,18 +14,62 @@
 
     public class IndexModel : PageModel
     {
+        #region Private Fields
+
         private readonly RazorPagesMovieContext _context;
+
+        #endregion Private Fields
+
+        #region Public Constructors
 
         public IndexModel(RazorPagesMovieContext context)
         {
             _context = context;
         }
 
+        #endregion Public Constructors
+
+        #region Public Properties
+
         public IList<Movie> Movie { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync().ConfigureAwait(false);
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            IQueryable<Movie> movies = from m in _context.Movie
+                                       select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync().ConfigureAwait(false));
+            Movie = await movies.ToListAsync().ConfigureAwait(false);
         }
+
+        #endregion Public Methods
     }
 }
