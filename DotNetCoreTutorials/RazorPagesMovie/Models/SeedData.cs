44a79fbe8c691bc306f2143ced45134bca28f570
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-04-02 12:04:04 +0200

    Seeding database

---
+++ DotNetCoreTutorials/RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,73 @@
+﻿namespace RazorPagesMovie.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using RazorPagesMovie.Data;
+    using System;
+    using System.Linq;
+
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            bool mustSaveChanges = false;
+
+            using RazorPagesMovieContext context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>());
+
+            if (context.Movie.Count(_ => _.Title.Equals("When Harry Met Sally")) == 0)
+            {
+                context.Movie.Add(new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                });
+                mustSaveChanges = true;
+            }
+
+            if (context.Movie.Count(_ => _.Title.Equals("Ghostbusters")) == 0)
+            {
+                context.Movie.Add(new Movie
+                {
+                    Title = "Ghostbusters",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                });
+                mustSaveChanges = true;
+            }
+
+            if (context.Movie.Count(_ => _.Title.Equals("Ghostbusters 2")) == 0)
+            {
+                context.Movie.Add(new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                });
+                mustSaveChanges = true;
+            }
+
+            if (context.Movie.Count(_ => _.Title.Equals("Rio Bravo")) == 0)
+            {
+                context.Movie.Add(new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+                });
+                mustSaveChanges = true;
+            }
+
+            if (mustSaveChanges)
+            {
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DotNetCoreTutorials/RazorPagesMovie/Program.cs
+++ DotNetCoreTutorials/RazorPagesMovie/Program.cs
@@ -1,19 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace RazorPagesMovie
 {
+    using System;
+
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    using RazorPagesMovie.Models;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
