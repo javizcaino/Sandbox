ce16b95d4a30e6de7ef7467c51bd003c5dd41b2a
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-04-02 11:42:42 +0200

    Added a model

---
+++ DotNetCoreTutorials/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿namespace RazorPagesMovie.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Movie
+    {
+        #region Public Properties
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        #endregion Public Properties
+    }
+}
