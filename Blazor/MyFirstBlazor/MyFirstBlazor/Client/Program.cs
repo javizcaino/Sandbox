132d09824b1a5b5dd3b226799be62020ac879cf2
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-11-30 18:02:21 +0100

    Added first Blazor sample application to playground

---
+++ Blazor/MyFirstBlazor/MyFirstBlazor/Client/Program.cs
@@ -0,0 +1,22 @@
+namespace MyFirstBlazor.Client
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync().ConfigureAwait(false);
+        }
+    }
+}
