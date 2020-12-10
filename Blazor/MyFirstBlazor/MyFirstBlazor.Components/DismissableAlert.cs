5b68bb2bdbafecd491146568136ee99c6b125f86
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2020-12-10 10:58:12 +0100

    Continue on samples and exercises

---
+++ Blazor/MyFirstBlazor/MyFirstBlazor.Components/DismissableAlert.cs
@@ -0,0 +1,35 @@
+﻿namespace MyFirstBlazor.Components
+{
+    using Microsoft.AspNetCore.Components;
+
+    public partial class DismissableAlert
+    {
+        private bool _show;
+
+        [Parameter]
+        public bool Show
+        {
+            get { return _show; }
+
+            set
+            {
+                if (_show != value)
+                {
+                    _show = value;
+                    ShowChanged.InvokeAsync(_show);
+                }
+            }
+        }
+
+        [Parameter]
+        public EventCallback<bool> ShowChanged { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public void Dismiss()
+        {
+            Show = false;
+        }
+    }
+}
---
+++ Blazor/MyFirstBlazor/MyFirstBlazor/Client/Shared/CounterData.cs
@@ -0,0 +1,25 @@
+﻿namespace MyFirstBlazor.Client.Shared
+{
+    using System;
+
+    public class CounterData
+    {
+        private int _count;
+
+        public int Count
+        {
+            get => _count;
+
+            set
+            {
+                if (_count != value)
+                {
+                    _count = value;
+                    CountChanged?.Invoke(_count);
+                }
+            }
+        }
+
+        public Action<int> CountChanged { get; set; }
+    }
+}
