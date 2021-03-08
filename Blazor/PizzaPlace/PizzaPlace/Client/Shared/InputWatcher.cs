de7f741d482d8ae9b71feb07d193dcccac2022e3
Juan Antonio Vizcaino Lopez <javizcaino@gmail.com>
2021-03-08 12:51:43 +0100

    NO sé lo que estoy guardando, pero voy a dejarlo guardado por si acaso

---
+++ Blazor/PizzaPlace/PizzaPlace/Client/Shared/InputWatcher.cs
@@ -0,0 +1,30 @@
+﻿namespace PizzaPlace.Client.Shared
+{
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Forms;
+
+    public class InputWatcher : ComponentBase
+    {
+        private EditContext _editContext;
+
+        [CascadingParameter]
+        public EditContext EditContext
+        {
+            get => _editContext;
+
+            set
+
+            {
+                _editContext = value;
+                EditContext.OnFieldChanged += async (sender, e) => await FieldChanged
+                    .InvokeAsync(e.FieldIdentifier.FieldName)
+                    .ConfigureAwait(false);
+            }
+        }
+
+        [Parameter]
+        public EventCallback<string> FieldChanged { get; set; }
+
+        public bool Validate() => EditContext?.Validate() ?? false;
+    }
+}
